bf69d62afa8105dd7e9209850fc6d9a9e2672066
xiaotong-sysu <linxt29@mail2.sysu.edu.cn>
2024-08-02 09:04:29 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
-# Train the intention encoder-decoder
+# Train the model on Task-I
 python train_PPT.py --info try_ST --gpu 3 --mode Short_term --learning_rate 0.001 --n_layer 3 --max_epochs 3000
 
-# Train the addressor, stage: warm up
+# Train the model on Task-II, stage: warm up
 python train_PPT.py --info try_Des_warm --gpu 0 --mode Des_warm --model_Pretrain ./training/Pretrained_Models/SDD/model_ST --learning_rate 0.001 --lambda_j 100
 
-# Train the addressor, stage: finetune
+# Train the model on Task-II, stage: finetune
 python train_PPT.py --info try_LT --gpu 2 --mode Long_term --model_Pretrain ./training/Pretrained_Models/SDD/model_Des_warm --learning_rate 0.0001 --lambda_j 100
 
-# Train the trajectory encoder-decoder
+# Train the modelon Task-III
 python train_PPT.py --info try_ALL --gpu 1 --mode ALL --model_Pretrain ./training/Pretrained_Models/SDD/model_LT --model_LT ./training/Pretrained_Models/SDD/model_LT --model_ST ./training/Pretrained_Models/SDD/model_ST --learning_rate 0.0015 --traj_lambda_soft 5 --des_lambda_soft 0.5 --lambda_desloss 1 --learning_rate_des 1e-7
