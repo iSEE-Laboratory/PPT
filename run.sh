26384f1681058bf69829dd3ce49c3e545187ec22
xiaotong-sysu <linxt29@mail2.sysu.edu.cn>
2024-10-01 11:22:10 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,10 +2,10 @@
 python train_PPT.py --info try_ST --gpu 0 --mode Short_term --learning_rate 0.001 --n_layer 3 --max_epochs 3000
 
 # Stage-II (warm-up): Train the model on Task-II
-python train_PPT.py --info try_Des_warm --gpu 0 --mode Des_warm --model_Pretrain ./training/Pretrained_Models/SDD/model_ST --learning_rate 0.001 --lambda_j 100
+python train_PPT.py --info try_Des_warm --gpu 0 --mode Des_warm --model_Pretrain ./training/Pretrained_Models/SDD/model_ST.ckpt --learning_rate 0.001 --lambda_j 100
 
 # Stage-II (finetune): Train the model on Task-II
-python train_PPT.py --info try_LT --gpu 0 --mode Long_term --model_Pretrain ./training/Pretrained_Models/SDD/model_Des_warm --learning_rate 0.0001 --lambda_j 100
+python train_PPT.py --info try_LT --gpu 0 --mode Long_term --model_Pretrain ./training/Pretrained_Models/SDD/model_Des_warm.ckpt --learning_rate 0.0001 --lambda_j 100
 
 # Stage-III: Train the modelon Task-III
-python train_PPT.py --info try_ALL --gpu 0 --mode ALL --model_Pretrain ./training/Pretrained_Models/SDD/model_LT --model_LT ./training/Pretrained_Models/SDD/model_LT --model_ST ./training/Pretrained_Models/SDD/model_ST --learning_rate 0.0015 --traj_lambda_soft 5 --des_lambda_soft 0.5 --lambda_desloss 1 --learning_rate_des 1e-7
+python train_PPT.py --info try_ALL --gpu 0 --mode ALL --model_Pretrain ./training/Pretrained_Models/SDD/model_LT.ckpt --model_LT ./training/Pretrained_Models/SDD/model_LT.ckpt --model_ST ./training/Pretrained_Models/SDD/model_ST.ckpt --learning_rate 0.0015 --traj_lambda_soft 5 --des_lambda_soft 0.5 --lambda_desloss 1 --learning_rate_des 1e-7
